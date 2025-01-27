156b94a12583ecbb813b31a15b70ab41ecee47ae
mark <furman.mark7887@gmail.com>
2025-01-27 21:30:29 +0500

    Добавлен основной файл с обучением и предиктом классов

---
+++ fit_and_predict.py
@@ -0,0 +1,144 @@
+from sentence_transformers import SentenceTransformer
+import os
+
+import torch
+import spacy
+
+import string
+
+from sklearn.neighbors import NearestNeighbors
+
+nlp = spacy.load("ru_core_news_sm")
+
+stop_words = nlp.Defaults.stop_words
+punctuations = string.punctuation
+
+
+def spacy_tokenizer(sentence: str) -> str:
+    """
+    :param sentence: текст для предобработки
+    :return: текст, состоящий из слов в начальной форме,
+    без знаков пунктуации, прописными буквами, без слов, не несущих никакого смысла
+    """
+    doc = nlp(sentence)
+
+    my_tokens = [word.lemma_.lower().strip() for word in doc]
+
+    my_tokens = [word for word in my_tokens if word not in stop_words and word not in punctuations]
+
+    sentence = " ".join(my_tokens)
+
+    return sentence
+
+
+class ClassificatorModel:
+    def __init__(self, model_name: str, threshold: int | float = 0.5, neighbors_amount: int = 3):
+        """
+        Класс модели, который содержит методы: \n
+        fit - обучение модели на основе существующих документов, разделенных на классы (по папкам!); \n
+        predict - классификация новых документов: распределение их по существующим классам (или класс Unknown в случае,
+        если не удалось определить принадлежность к конкретному классу) при помощи алгоритма ближайших соседей; \n
+        score - вычисление доли точных (верных) предсказаний модели.
+        :param model_name: Ссылка на модель HuggingFace
+        :param threshold: Порог, после которого новый документ не будет считаться схожим с существующими
+        :param neighbors_amount: Количество соседних документов для сравнения.
+        """
+        self.model = SentenceTransformer(model_name,
+                                         similarity_fn_name="cosine",
+                                         prompts={
+                                             "clustering": "Определи тематику и основную мысль текста."})
+        self.embeddings: list = []
+        self.labels: list = []
+        self.grouped_files: dict = {}
+        self.threshold: torch.Tensor = torch.Tensor([threshold])
+        self.dictionary_length: int = -1
+        self.knn = NearestNeighbors(n_neighbors=neighbors_amount, metric='cosine')
+
+    def fit(self, path_to_dir: str) -> None:
+        """
+        Метод для обучения модели. Считывает директорию с папками (т.е. классами) и текстовыми документами в них.
+        Предобрабатывает их и считает эмбеддинги.
+        :param path_to_dir: Путь до директории с классами.
+        :return: None.
+        """
+        for label in os.listdir(path_to_dir):
+            label_dir = os.path.join(path_to_dir, label)
+            if os.path.isdir(label_dir):
+                for filename in os.listdir(label_dir):
+                    filepath = os.path.join(label_dir, filename)
+                    try:
+                        with open(filepath, 'r', encoding='utf-8') as f:
+                            text = spacy_tokenizer(f.read())
+                            embedding = self.model.encode(text)
+                            self.embeddings.append(embedding)
+                            self.labels.append(label)
+                    except UnicodeDecodeError as e:
+                        print(f"Ошибка при чтении файла {filepath}: {e}")
+                        continue
+
+        self.knn.fit(self.embeddings)
+
+        return None
+
+    def predict(self, path_to_dir: str) -> list[str]:
+        """
+        Метод для предсказания меток классов новых документов. Получает на вход директорию с текстами.
+        Предобрабатывает их и считает эмбеддинги. Далее проверка принадлежности к существующим классам происходит при
+        помощи kNN.
+        :param path_to_dir: Путь до папки с текстами.
+        :return: Предсказанные метки классов в виде ["Метка класса1", "Метка класса2", ...]
+        """
+        new_embeddings = []
+
+        for file in os.listdir(path_to_dir):
+            with open(f"{path_to_dir}/{file}", 'r', encoding='utf-8') as f:
+                text = spacy_tokenizer(f.read())
+                new_embeddings.append(self.model.encode(text))
+
+        # print(knn.n_samples_fit_)
+        predicted_labels = []
+        for new_embedding in new_embeddings:
+            distances, indices = self.knn.kneighbors([new_embedding])
+            print(distances, indices)
+            if distances[0][0] > self.threshold:
+                predicted_labels.append("Unknown")
+            else:
+                predicted_label = self.labels[indices[0][0]]
+                predicted_labels.append(predicted_label)
+        return predicted_labels
+
+    def score(self, predicted: list[str], true: list[str]) -> None | str:
+        """
+        Метод для вычисления доли верно предсказанных меток классов.
+        :param predicted: Предсказанные метки.
+        :param true: Ground truth метки.
+        :return: Доля верно предсказанных классов.
+        """
+        pred_length = len(predicted)
+        true_length = len(true)
+
+        counter = 0
+
+        if pred_length != true_length:
+            raise ValueError(
+                f"Длина предсказанных меток ({pred_length}) не соответствует длине правильных меток ({true_length}).")
+
+        else:
+            for i, v in enumerate(predicted):
+                if v == true[i]:
+                    counter += 1
+
+            return f'Доля верно предсказанных классов: {round(counter / pred_length * 100, 2)}%'
+
+
+model = ClassificatorModel("cointegrated/rubert-tiny2", 0.2)
+
+model.fit("texts_by_classes")
+
+predicted = model.predict("texts_to_classify")
+
+print(predicted)
+
+true = ['News', 'DescriptionTexts', 'DescriptionTexts', 'City', 'DescriptionTexts', 'News']
+
+print(model.score(predicted, true))
